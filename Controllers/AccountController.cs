b9977edda93f33113f71531c340728731ac2c483
Ines IZERE <izere.ines@gmail.com>
2021-04-11 22:24:03 -0400

    plzcheck

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace rocket_elevator_ui.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-        private string url = "https://rocketcustomer.herokuapp.com/";
+        private string url = "https://localhost:5001";
 
         public AccountController()
         {
